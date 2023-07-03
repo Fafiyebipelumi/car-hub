3c198d972f2517c8e9b64aaae178cc522304b2f1
Oluwapelumi <fafyeepelumi04@gmail.com>
2023-07-03 10:06:30 -0700

    latest commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,25 @@
-import { Navbar } from "./components";
+"use client";
+
+import { UseQueryResult, useQuery } from "@tanstack/react-query";
+// import { Navbar } from "./components";
+import axios from "axios";
+import { ITransactionResponse } from "./interfaces/transaction.interface";
+
 
 export default function Home() {
+  async function fetchTransactions(): Promise<ITransactionResponse> {
+    const response = await axios.get('http://jsonplaceholder.typicode.com/users')
+    return response.data
+  }
+
+  const { data, isLoading } = useQuery({
+    queryKey: ['transaction'],
+    queryFn: fetchTransactions
+  })
   return (
-    <Navbar>
-      <h1>Welcome to the homepage</h1>
-      <p>Content of your homepage goes here.</p>
-    </Navbar>
+    <div>
+      {isLoading && <p>Loading...</p>}
+      <h3 style={{ color: 'black' }}>{data?.id} - {data?.username}</h3>
+    </div>
   )
 }
